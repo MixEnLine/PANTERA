ba1cf9a82f9a62f7a220f0be27e66dd0562c5daf
MixEnLine <mixenline@gmail.com>
2025-04-21 16:32:36 +0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    uiPort: process.env.PORT || 10000,
+    uiHost: "0.0.0.0",
+    credentialSecret: false,
+    editorTheme: {
+        projects: {
+            enabled: false
+        }
+    }
+};
